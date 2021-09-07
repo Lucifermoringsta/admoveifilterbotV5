62474131e97cb8deeb7bf56f7e6c650306f3b1bb
Lucifermoringsta <90172375+Lucifermoringsta@users.noreply.github.com>
2021-09-07 18:43:50 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -10,54 +10,4 @@ class Translation(object):
 <b>🤭എന്നെ നിങ്ങൾക്ക് വേറെ ഒരിടത്തും ഉപയോഗിക്കാൻ ആകില്ല..😂😂😂<b>
 <b>😂അങ്ങനെ നിങൾ ഉപയോഗിക്കാൻ നോക്കിയാൽ Error❌ വരും അങ്ങനെ Error വരുമ്പോൾ നിങൾ വീണ്ടും Try ചെയ്യും അങ്ങനെ ചെയ്ത് ചെയ്ത്
 നിങ്ങളുടെ തല ചൂടാകും..
-<b>🤭തല ചൂടായാൽ നിങൾ എന്തും ചെയ്യും അപ്പോ ജനം പറയും നിങ്ങൾക്ക് ഭ്രാന്ത് ആണെന്ന് ഭ്രാന്ത്😂😂<b>
-
-
-
-
-
-
-
-
-<i>
--> Add Me To Any Group And Make Me Admin
--> Add Me To Your Desired Channel
-</i>
-
-<b>Bot Commands (Works Only In Groups) :</b>
-
-    -> <code>/add chat_id</code>
-                OR                  - <b>To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)</b>
-     <code>/add @Username</code>
-     
-    -> <code>/del chat_id</code>
-                OR                  - <b>To disconnect A Group With A Channel</b>
-     <code>/del @Username</code>
-     
-    -> <code>/delall</code>  - <b>This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB</b>
-    
-    -> <code>/settings</code> -  <b>This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly</b>
-
-            -> <code>Channel</code> - <b>Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls</b>
-            
-            -> <code>Filter Types</code> - <b>Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart</b>
-
-            -> <code>Configure</code> - <b>Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results</b>
-            
-            -> <code>Status</code> - <b>Button Will Shows The Stats Of Your Channel</b>
-            
-<b><a href="https://t.me/Mo_Tech_YT">© Mo Tech YT</a></b>
-"""
-    
-    ABOUT_TEXT = """<b>➥ 📒Name</b> : <b>Pro Auto Filter Bot</b>
-
-<b>>➥⭕️Master</b> : <b><a href="https://t.me/AlbertEinstein_TG">👤Albert Einstein</a></b>
-
-<b>➥ ⭕️Editor</b> : <b><a href="https://t.me/MRk_YT">👤Mrk YT</a></b>
-
-<b>➥ 🗣️Language</b> : <b>Python3<b>
-
-<b>➥ 📒Library</b> : <b><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></b>
-
-<b>➥ 📂Source Code</b> : <b><a href="https://github.com/MoTechYT/Pro-Auto-Filter-Bot-V2">💥Click Me</a></b>
-"""
+<b>🤭തല ചൂടായാൽ നിങൾ എന്തും ചെയ്യും അപ്പോ ജനം പറയും നിങ്ങൾക്ക് ഭ്രാന്ത് ആണെന്ന് ഭ്രാന്ത്😂<b>
